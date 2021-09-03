655a9e3e59cf5899eee70d72a405b539770e1640
bigelowd-erau <88133216+bigelowd-erau@users.noreply.github.com>
2021-09-03 13:20:56 -0700

    Update Menu.cs

--- Cubethon/Assets/Scripts/Menu.cs
+++ Cubethon/Assets/Scripts/Menu.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    //called when start button on menue screen is pressed
+    //starts by loading scene next in the build order
     public void startGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
