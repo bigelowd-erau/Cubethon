cb931426b1e7e39c0ecb2ef235995511aa3e3d04
bigelowd-erau <88133216+bigelowd-erau@users.noreply.github.com>
2021-09-03 13:16:27 -0700

    Update GameManager.cs

--- Cubethon/Assets/Scripts/GameManager.cs
+++ Cubethon/Assets/Scripts/GameManager.cs
@@ -5,25 +5,35 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
+    //the time in seconds delay before causing the scene to reset
     public float restartDelay = 1f;
+    //the complete level panel
     public GameObject completeLevelScreen;
-
+    
+    //called when the end object is triggered
     public void CompleteLevel()
     {
+        //enables the level complete panel
+        //causing the panel to overlay the game
         completeLevelScreen.SetActive(true);
     }
-
+    
+    //Called when the player loses the level
+    //hits obstacle or falls off map
     public void EndGame()
     {
+        //checks game has not previously quit    
         if (gameHasEnded == false)
         {
+            //set game as ended
             gameHasEnded = true;
-            Debug.Log("Game Over!");
+            //call the restart function after the defined restart delay
             Invoke("Restart", restartDelay);
         }
         
     }
 
+    //reloads the current level
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
