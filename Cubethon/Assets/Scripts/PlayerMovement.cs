2f5aa792f15ded4d55befdcb0d5d30517e1da2ec
bigelowd-erau <88133216+bigelowd-erau@users.noreply.github.com>
2021-09-03 13:29:36 -0700

    Update PlayerMovement.cs

--- Cubethon/Assets/Scripts/PlayerMovement.cs
+++ Cubethon/Assets/Scripts/PlayerMovement.cs
@@ -4,44 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //the player object's rigid body
     public Rigidbody rb;
     public float forwardForce;
     public float sidewaysforce;
-    public float jumpForce;
-    public float jumpCoolDown;
-    private float currentJumpCooldown = 0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
+    // Fixedupdate is called at a fixed time interval
     void FixedUpdate()
     {
+        //adds a consistent forward force to the player object
         rb.AddForce(0,0,forwardForce * Time.deltaTime);
-
+        
+        //if d is pressed on the keyboard
         if (Input.GetKey("d"))
-        {
-        rb.AddForce(sidewaysforce*Time.deltaTime,0,0, ForceMode.VelocityChange);
+        {   
+            //adds a velocity change force to the player relative right 
+            rb.AddForce(sidewaysforce*Time.deltaTime,0,0, ForceMode.VelocityChange);
         }
+        //if a is pressed on the keyboard
         if (Input.GetKey("a"))
         {
-        rb.AddForce(-sidewaysforce*Time.deltaTime,0,0, ForceMode.VelocityChange);
+            //adds a velocity change force to the player relative left
+            rb.AddForce(-sidewaysforce*Time.deltaTime,0,0, ForceMode.VelocityChange);
         }
-        /*if (Input.GetKey("space"))
-        {
-            if (currentJumpCooldown < 0)
-            {
-                rb.AddForce(0,jumpForce, 0, ForceMode.Impulse);
-                currentJumpCooldown = jumpCoolDown;
-            } else {
-                --currentJumpCooldown;
-            }
-        }*/
-
+        //check if the player has fallen off the map
         if (rb.position.y < -1f)
         {
+            //reset the level
             FindObjectOfType<GameManager>().EndGame();
         }
     }
